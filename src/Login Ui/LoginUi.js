62be6d76295bd895cd3973f32e75ccaa494f427d
jayasurya018 <jayasuryavs1999@gmail.com>
2022-04-22 12:25:53 +0530

    login updates

--- src/Login Ui/LoginUi.js
+++ src/Login Ui/LoginUi.js
@@ -1,44 +1,45 @@
-import './LoginUi.css';
+import "./LoginUi.css";
 import profile from "./../image/a.png";
 import email from "./../image/email.jpg";
 import pass from "./../image/pass.png";
 function LoginUi() {
   return (
     <div className="main">
-     <div className="sub-main">
-       <div>
-         <div className="imgs">
-           <div className="container-image">
-             <img src={profile} alt="profile" className="profile"/>
-
-           </div>
-
-
-         </div>
-         <div>
-           <h1>Login Page</h1>
-           <div>
-             <img src={email} alt="email" className="email"/>
-             <input type="text" placeholder="Enter the Email" className="name"/>
-           </div>
-           <div className="second-input">
-             <img src={pass} alt="pass" className="email"/>
-             <input type="password" placeholder="Password" className="name"/>
-           </div>
-          <div className="login-button">
-          <button>Login</button>
+      <div className="sub-main">
+        <div>
+          <div className="imgs">
+            <div className="container-image">
+              <img src={profile} alt="profile" className="profile" />
+            </div>
           </div>
-           
+          <div>
+            <h1>Login Page</h1>
+            <div>
+              <img src={email} alt="email" className="email" />
+              <input
+                type="text"
+                placeholder="Enter your Email"
+                className="name"
+              />
+            </div>
+            <div className="second-input">
+              <img src={pass} alt="pass" className="email" />
+              <input
+                type="password"
+                placeholder=" Enter your Password"
+                className="name"
+              />
+            </div>
+            <div className="login-button">
+              <button type="submit">Login</button>
+            </div>
+
             <p className="link">
               <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
             </p>
-           
- 
-         </div>
-       </div>
-       
-
-     </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
